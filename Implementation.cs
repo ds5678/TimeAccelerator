3811dd0256e61d76e63313b3a1df2f3c48ade4e4
ds5678 <jrpritts7@gmail.com>
2021-04-12 18:08:07 -0400

    updated to use ModSettings 1.8

--- Implementation.cs
+++ Implementation.cs
@@ -1,50 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Harmony;
 using MelonLoader;
-using Harmony;
 using UnityEngine;
 
 namespace TimeAccelerator
 {
-    public class Implementation : MelonMod
-    {
-        public override void OnApplicationStart()
-        {
-            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
-            Settings.OnLoad();
-        }
-    }
+	public class Implementation : MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+			Settings.OnLoad();
+		}
+	}
 
-    [HarmonyPatch(typeof(InterfaceManager), "Update")]
-    public class TimeAcceleratorUpdate
-    {
-        static bool accelerated = false;
-        public static void Prefix(HUDManager __instance)
-        {
-            if (Settings.holdToAccelerate)
-            {
-                if (KeyboardUtilities.InputManager.GetKey(Settings.accelerationKey))
-                {
-                    Time.timeScale = Settings.accelerationValue;
-                }
-                else
-                {
-                    Time.timeScale = 1.0f;
-                }
-            }
-            else
-            {
-                if (KeyboardUtilities.InputManager.GetKeyDown(Settings.accelerationKey))
-                {
-                    accelerated = !accelerated;
-                }
+	[HarmonyPatch(typeof(InterfaceManager), "Update")]
+	public class TimeAcceleratorUpdate
+	{
+		private static bool accelerated = false;
+		public static void Prefix()
+		{
+			if (InterfaceManager.IsOverlayActiveCached()) return;
+			if (Settings.options.accelerationBehaviour == 0)
+			{
+				if (KeyboardUtilities.InputManager.GetKey(Settings.options.accelerationKey))
+				{
+					Time.timeScale = Settings.options.accelerationValue;
+				}
+				else
+				{
+					Time.timeScale = 1.0f;
+				}
+			}
+			else
+			{
+				if (KeyboardUtilities.InputManager.GetKeyDown(Settings.options.accelerationKey))
+				{
+					accelerated = !accelerated;
+				}
 
-                if (accelerated) Time.timeScale = Settings.accelerationValue;
-                else             Time.timeScale = 1.0f;
-            }
-        }
-    }
+				if (accelerated) Time.timeScale = Settings.options.accelerationValue;
+				else Time.timeScale = 1.0f;
+			}
+		}
+	}
 }
--- Settings.cs
+++ Settings.cs
@@ -1,48 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ModSettings;
+﻿using ModSettings;
 using UnityEngine;
 
 namespace TimeAccelerator
 {
-    internal class TimeAcceleratorSettings : JsonModSettings
-    {
-        [Name("Acceleration key")]
-        [Choice("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")]
-        public int accelerationKey = 1;
+	internal class TimeAcceleratorSettings : JsonModSettings
+	{
+		[Name("Acceleration Key")]
+		public KeyCode accelerationKey = KeyCode.B;
 
-        [Name("Acceleration behaviour")]
-        [Choice("Hold to accelerate", "Toggle on press")]
-        public int accelerationBehaviour = 0;
+		[Name("Acceleration Behaviour")]
+		[Choice("Hold to accelerate", "Toggle on press")]
+		public int accelerationBehaviour = 0;
 
-        [Name("Acceleration speed")]
-        [Description("1 = 100%, 2 = 200%")]
-        [Slider(0f, 10f, 101)]
-        public float accelerationValue = 3f;
-
-        protected override void OnConfirm()
-        {
-            base.OnConfirm();
-            Settings.accelerationKey = KeyCode.A + accelerationKey;
-            Settings.holdToAccelerate = accelerationBehaviour == 0;
-            Settings.accelerationValue = accelerationValue;
-        }
-    }
+		[Name("Acceleration Speed")]
+		[Description("1 = 100%, 2 = 200%")]
+		[Slider(0f, 10f, 101)]
+		public float accelerationValue = 3f;
+	}
 
 
-    internal static class Settings
-    {
-        internal static KeyCode accelerationKey = KeyCode.V;
-        internal static float accelerationValue = 3.0f;
-        internal static bool holdToAccelerate = false;
-        private static TimeAcceleratorSettings options;
-        internal static void OnLoad()
-        {
-            options = new TimeAcceleratorSettings();
-            options.AddToModSettings("Time Accelerator", MenuType.Both);
-        }
-    }
+	internal static class Settings
+	{
+		internal static TimeAcceleratorSettings options;
+		internal static void OnLoad()
+		{
+			options = new TimeAcceleratorSettings();
+			options.AddToModSettings("Time Accelerator", MenuType.Both);
+		}
+	}
 }
